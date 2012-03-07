ae79999f0f007ebfbb6c231ac925059436358722
fmo <fathmoummou@yahoo.fr>
2012-03-07 12:10:04 +0100

    create business component

---
+++ src/main/java/com/mine/project/service/FirstService.java
@@ -0,0 +1,6 @@
+package com.mine.project.service;
+
+public interface FirstService {
+
+	void doSomeThing();
+}
---
+++ src/main/java/com/mine/project/service/impl/FirstServiceImpl.java
@@ -0,0 +1,13 @@
+package com.mine.project.service.impl;
+
+import com.mine.project.service.FirstService;
+
+public class FirstServiceImpl implements FirstService {
+
+	
+	public void doSomeThing() {	
+      System.out.println("doing something .....");
+      System.out.println("something done"); 
+	}
+
+}
